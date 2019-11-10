b885e6b87e064a975314547c7ba135d22efc5a96
James Birtles <jameshbirtles@gmail.com>
2019-11-10 18:06:48 +0000

    fix windows code

--- src/windows.rs
+++ src/windows.rs
@@ -1,21 +1,20 @@
 use std::collections::HashMap;
 use std::mem;
-use std::os::raw::c_ulong;
-use std::ptr;
-use winapi::shared::windef::{HWND, POINT};
+use winapi::shared::windef::HWND;
 use winapi::um::winuser;
 
 pub mod modifiers {
     use winapi::um::winuser;
-    pub const ALT: u32 = winuser::MOD_ALT;
-    pub const CONTROL: u32 = winuser::MOD_CONTROL;
-    pub const SHIFT: u32 = winuser::MOD_SHIFT;
-    pub const SUPER: u32 = winuser::MOD_WIN;
+    pub const ALT: u32 = winuser::MOD_ALT as u32;
+    pub const CONTROL: u32 = winuser::MOD_CONTROL as u32;
+    pub const SHIFT: u32 = winuser::MOD_SHIFT as u32;
+    pub const SUPER: u32 = winuser::MOD_WIN as u32;
 }
 
 pub type ListenerID = i32;
 
 pub struct Listener {
+    last_id: i32,
     handlers: HashMap<ListenerID, Box<dyn Fn()>>,
 }
 
@@ -34,7 +33,8 @@ impl Listener {
         handler: CB,
     ) -> Result<ListenerID, String> {
         unsafe {
-            let id = self.last_id += 1;
+            self.last_id += 1;
+            let id = self.last_id;
             let result = winuser::RegisterHotKey(0 as HWND, id, modifiers, key);
             if result == 0 {
                 return Err("Failed to register hotkey".to_string());
@@ -48,14 +48,7 @@ impl Listener {
     pub fn listen(self) {
         unsafe {
             loop {
-                let mut msg = winuser::MSG {
-                    hwnd: 0 as HWND,
-                    message: 0,
-                    wParam: 0,
-                    lParam: 0,
-                    time: 0,
-                    pt: POINT { x: 0, y: 0 },
-                };
+                let mut msg = mem::MaybeUninit::uninit().assume_init();
                 while winuser::GetMessageW(&mut msg, 0 as HWND, 0, 0) > 0 {
                     if msg.wParam != 0 {
                         if let Some(handler) = self.handlers.get(&(msg.wParam as i32)) {
